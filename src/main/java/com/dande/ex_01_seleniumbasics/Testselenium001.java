59ee3b0f817ea121417f37ff0580e2c8f27b8639
AkhilDande <venkataakhildande@gmail.com>
2025-03-23 14:30:18 +0530

    March 23 2025

---
+++ src/main/java/com/dande/ex_01_seleniumbasics/Testselenium001.java
@@ -0,0 +1,16 @@
+package com.dande.ex_01_seleniumbasics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class Testselenium001 {
+        @Test
+         public void test_Verify_VWO_login()
+        {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com/");
+        System.out.println(driver.getTitle());
+
+        }
+}
